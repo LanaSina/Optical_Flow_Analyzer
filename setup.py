f4a9791e1d053fb5440aeb97909ab8c5a1fec5c2
Lana <lana.sinapayen@gmail.com>
2020-06-03 11:27:29 +0900

    no message

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chainer_prednet',
+    version='1.0.0',
+    url='https://github.com/LanaSina/chainer_prednet.git',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),
+)
